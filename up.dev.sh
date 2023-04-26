3ac9a7314a8f26c0efb22f9c880546193c750bd5
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2023-04-26 05:32:09 -0300

    chore: add up bash script

---
+++ up.dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function trap_ctrlc ()
+{
+    # perform cleanup here
+    docker-compose -f docker-compose.dev.yml down
+ 
+    # exit shell script with error code 2
+    # if omitted, shell script will continue execution
+    exit 2
+}
+
+trap "trap_ctrlc" 2
+
+if [ ! -e "app.log" ] ; then
+    touch "app.log"
+fi
+
+docker-compose -f docker-compose.dev.yml --env-file .env.dev up -d --build --remove-orphans
+docker-compose -f docker-compose.dev.yml logs -f --tail=15 api
